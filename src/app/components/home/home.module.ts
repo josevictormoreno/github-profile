3eaa2b06ff426c6d292569a811b41c1c7f08d13e
josevictormoreno <josevictor.admoreno@gmail.com>
2022-12-21 23:19:52 -0300

    adiciona toolbar

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,14 +2,12 @@
 
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
-import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
     HomeComponent,
   ],
   imports: [
-    MatToolbarModule,
   ]
 })
 export class HomeModule { }
